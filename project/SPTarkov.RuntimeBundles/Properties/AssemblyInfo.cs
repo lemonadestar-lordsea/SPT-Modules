ad61147697a53975f08038d26a7163d43d97f0e4
waffle.lord <waffle.lord@hotmail.com>
2020-10-20 19:56:19 -0400

    revert and fix this one file

--- project/SPTarkov.RuntimeBundles/Properties/AssemblyInfo.cs
+++ project/SPTarkov.RuntimeBundles/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-/* AssemblyInfo.cs
+﻿/* AssemblyInfo.cs
  * License: NCSA Open Source License
  * 
  * Copyright: Merijn Hendriks
@@ -6,40 +6,39 @@
  * Merijn Hendriks
  */
 
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// ?????????????
-// ?????????????
-// ??????????
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
 [assembly: AssemblyTitle("SPTarkov.RuntimeBundles")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SPTarkov.RuntimeBundles")]
-[assembly: AssemblyCopyright("Copyright �  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ? ComVisible ??? false ??????????
-//? COM ??????????? COM ??????????
-//?????? ComVisible ????? true?
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// ?????? COM ??,??? GUID ?????? ID
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("dd923a53-4605-49ce-af1d-b61137d0e4a3")]
 
-// ????????????????: 
+// 程序集的版本信息由下列四个值组成: 
 //
-//      ???
-//      ???
-//      ???
-//      ???
+//      主版本
+//      次版本
+//      生成号
+//      修订号
 //
-// ???????,?????????? "*" ?????????
-//???? "*",????:
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+//通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
