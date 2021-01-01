f67a85b8f73055180e8c48c487d44ea5760fafc4
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-01 03:38:02 +0100

    WIP attempt 2

--- project/Aki.Core/Patches/NotifierDefaultPatch.cs
+++ project/Aki.Core/Patches/NotifierDefaultPatch.cs
@@ -15,7 +15,7 @@ using Aki.Common.Utils.Patching;
 namespace Aki.Core.Patches
 {
 	/* In patch 0.12.9.10510 they did a small refactor to ApplyTansportType to default to websocket.
-	 * With this patch we always force to use HTTP notifier regardless of the user or game defaults.
+	 * With this patch we force to use HTTP notifier on startup regardless of the game defaults.
 	 * Targeted method is unique and only appears in GClass1328
 	 */
 
@@ -24,6 +24,7 @@ namespace Aki.Core.Patches
 		public NotifierDefaultPatch() : base(prefix: nameof(PatchPrefix)) { }
 
 		public static Type __type;
+		public static FieldInfo __channel;
 
 		protected override MethodBase GetTargetMethod()
 		{
@@ -31,15 +32,17 @@ namespace Aki.Core.Patches
 			var flags = BindingFlags.Public | BindingFlags.Instance;
 			
 			__type = PatcherConstants.TargetAssembly.GetTypes().Single(x => x.GetMethod(methodName, flags) != null);
+			__channel = __type.GetField($"{typeof(GClass792).Name.ToLower()}_0", BindingFlags.NonPublic | BindingFlags.Instance);
+
 			return __type.GetMethod(methodName, flags);
 		}
 
 		static bool PatchPrefix(ref object __instance, ENotificationTransportType transportType)
 		{
-			if (transportType != ENotificationTransportType.Default)
+			if (transportType == ENotificationTransportType.Default && __channel.GetValue(__instance) == null)
 			{
-				__type.GetMethod("ApplyTransportType").Invoke(__instance, new object[] { ENotificationTransportType.Default });
-				return false;
+				// game requires websock to run at least once before we can initialize HTTP notifier
+				__type.GetMethod("ApplyTransportType").Invoke(__instance, new object[] { ENotificationTransportType.WebSocket });
 			}
 
 			return true;
