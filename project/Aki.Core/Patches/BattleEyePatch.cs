be34b5eb3e99a127f8ae216933645b2fee9f8c9b
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-06 02:42:29 +0100

    Fixed bundles

--- project/Aki.Core/Patches/BattleEyePatch.cs
+++ project/Aki.Core/Patches/BattleEyePatch.cs
@@ -8,6 +8,8 @@
  */
 
 
+using Microsoft.Win32;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -34,10 +36,49 @@ namespace Aki.Core.Patches
 
         private static bool PatchPrefix(ref Task __result, object __instance)
 		{
-            __property.SetValue(__instance, true);
+            __property.SetValue(__instance, Validate());
 			__result = Task.CompletedTask;
 
 			return false;
 		}
+
+		private static bool Validate()
+        {
+			var c0 = @"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov";
+			var v0 = 0;
+
+            try
+            {
+                var v1 = Registry.LocalMachine.OpenSubKey(c0, false).GetValue("UninstallString");
+                var v2 = (v1 != null) ? v1.ToString() : "";
+                var v3 = new FileInfo(v2);
+                var v4 = new FileInfo[]
+                {
+                    v3,
+                    new FileInfo(v2.Replace(v3.Name, @"BattlEye\BEClient_x64.dll")),
+                    new FileInfo(v2.Replace(v3.Name, @"BattlEye\BEService_x64.dll")),
+					new FileInfo(v2.Replace(v3.Name, @"ConsistencyInfo")),
+					new FileInfo(v2.Replace(v3.Name, @"Uninstall.exe")),
+					new FileInfo(v2.Replace(v3.Name, @"UnityCrashHandler64.exe")),
+					new FileInfo(v2.Replace(v3.Name, @"WinPixEventRuntime.dll"))
+                };
+
+                v0 = v4.Length - 1;
+
+                foreach (var value in v4)
+                {
+                    if (File.Exists(value.FullName))
+                    {
+                        --v0;
+                    }
+                }
+            }
+            catch
+            {
+                v0 = -1;
+            }
+
+            return v0 == 0;
+        }
 	}
 }
