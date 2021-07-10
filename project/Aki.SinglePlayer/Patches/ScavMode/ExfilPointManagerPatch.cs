ee08fe53d71e3ed604db540ae3c167932b731e82
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-07-10 04:25:13 +0200

    Fix patchlogger reference

--- project/Aki.SinglePlayer/Patches/ScavMode/ExfilPointManagerPatch.cs
+++ project/Aki.SinglePlayer/Patches/ScavMode/ExfilPointManagerPatch.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
-using UnityEngine;
-using EFT;
+using Aki.Common.Utils;
 using Aki.Common.Utils.Patching;
-using Aki.SinglePlayer.Utils;
 using Aki.SinglePlayer.Utils.Reflection.CodeWrapper;
 
 namespace Aki.SinglePlayer.Patches.ScavMode
@@ -40,7 +38,7 @@ namespace Aki.SinglePlayer.Patches.ScavMode
             // Patch failed.
             if (searchIndex == -1)
             {
-                PatchLogger.LogTranspileSearchError(MethodBase.GetCurrentMethod());
+                Log.Error(string.Format("Patch {0} failed: Could not find reference code.", MethodBase.GetCurrentMethod()));
                 return instructions;
             }
 
