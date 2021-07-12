692793c925496220e2c31afe7a0b4b5fee49581b
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-07-12 16:41:38 +0200

    Fix RemoveUsedBotProfilePatch

--- project/Aki.SinglePlayer/Patches/Bots/RemoveUsedBotProfilePatch.cs
+++ project/Aki.SinglePlayer/Patches/Bots/RemoveUsedBotProfilePatch.cs
@@ -11,6 +11,7 @@ namespace Aki.SinglePlayer.Patches.Bots
 {
     public class RemoveUsedBotProfilePatch : GenericPatch<RemoveUsedBotProfilePatch>
     {
+        private static BindingFlags _flags;
         private static Type _targetInterface;
         private static Type _targetType;
         private static FieldInfo _profilesField;
@@ -22,14 +23,15 @@ namespace Aki.SinglePlayer.Patches.Bots
 
         public RemoveUsedBotProfilePatch() : base(prefix: nameof(PatchPrefix))
         {
+            _flags = BindingFlags.Instance | BindingFlags.NonPublic;
             _targetInterface = Constants.EftTypes.Single(IsTargetInterface);
             _targetType = Constants.EftTypes.Single(IsTargetType);
-            _profilesField = _targetType.GetField("list_0");
+            _profilesField = _targetType.GetField("list_0", _flags);
         }
 
         protected override MethodBase GetTargetMethod()
         {
-            return _targetType.GetMethod("GetNewProfile", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            return _targetType.GetMethod("GetNewProfile", _flags);
         }
 
         private static bool IsTargetInterface(Type type)
