60ff6b1ff07241dc5287b0d573b3c019fb80d339
Martynas Gestautas <martgest@gmail.com>
2021-07-14 11:57:59 +0300

    Cleaned up patch code

--- project/Aki.SinglePlayer/Patches/Dev/CoordinatesPatch.cs
+++ project/Aki.SinglePlayer/Patches/Dev/CoordinatesPatch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using UnityEngine;
 using TMPro;
@@ -10,43 +11,42 @@ namespace Aki.Singleplayer.Patches.Dev
 {
     public class CoordinatesPatch : GenericPatch<CoordinatesPatch>
     {
-        private static TextMeshProUGUI alphaLabel = null;
-        public static PropertyInfo _playerProperty;
-        public static PropertyInfo _uiProperty;
-        public static bool firstTime = true;
+        private static TextMeshProUGUI _alphaLabel;
+        private static PropertyInfo _playerProperty;
 
         public CoordinatesPatch() : base(prefix: nameof(PrefixPatch))
         {
         }
 
-        static void PrefixPatch(object __instance)
+        private static void PrefixPatch(object __instance)
         {
-			if (firstTime)
-			{
-				alphaLabel = GameObject.Find("AlphaLabel").GetComponent<TextMeshProUGUI>();
-				alphaLabel.color = Color.green;
-				alphaLabel.fontSize = 22;
-				alphaLabel.font = Resources.Load<TMP_FontAsset>("Fonts & Materials/ARIAL SDF");
-				firstTime = false;
-			}
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                if (_alphaLabel == null)
+                {
+                    _alphaLabel = GameObject.Find("AlphaLabel").GetComponent<TextMeshProUGUI>();
+                    _alphaLabel.color = Color.green;
+                    _alphaLabel.fontSize = 22;
+                    _alphaLabel.font = Resources.Load<TMP_FontAsset>("Fonts & Materials/ARIAL SDF");
+                }
 
-            var playerOwner = (GamePlayerOwner)_playerProperty.GetValue(__instance);
-            var aiming = LookingRaycast(playerOwner.Player);
-			bool keyDown = Input.GetKeyDown(KeyCode.LeftControl);
-            string PlayerPosition = null;
+                var playerOwner = (GamePlayerOwner)_playerProperty.GetValue(__instance);
+                var aiming = LookingRaycast(playerOwner.Player);
 
-			if (alphaLabel != null && keyDown)
-				alphaLabel.text = $"Looking at: [{aiming.x}, {aiming.y}, {aiming.z}]";
-                PlayerPosition = $"Here is the character position: [{playerOwner.transform.position.x},{playerOwner.transform.position.y},{playerOwner.transform.position.z},{playerOwner.transform.rotation}]";
-                Log.Info(alphaLabel.text);
-                Log.Info(PlayerPosition);
+                if (_alphaLabel != null)
+                {
+                    _alphaLabel.text = $"Looking at: [{aiming.x}, {aiming.y}, {aiming.z}]";
+                    Log.Info(_alphaLabel.text);
+                }
+
+                Log.Info($"Here is the character position: [{playerOwner.transform.position.x},{playerOwner.transform.position.y},{playerOwner.transform.position.z},{playerOwner.transform.rotation}]");
+            }
         }
 
         protected override MethodBase GetTargetMethod()
         {
             var localGameBaseType = Constants.LocalGameType.BaseType;
             _playerProperty = localGameBaseType.GetProperty("PlayerOwner", BindingFlags.Public | BindingFlags.Instance);
-            _uiProperty = localGameBaseType.GetProperty("GameUi", BindingFlags.NonPublic | BindingFlags.Instance);
             return localGameBaseType.GetMethod("Update", Constants.PrivateFlags);
         }
 
@@ -54,15 +54,17 @@ namespace Aki.Singleplayer.Patches.Dev
         {
             try
             {
-                if (player != null && player.Fireport == null)
+                if (player == null || player.Fireport == null)
+                {
                     return Vector3.zero;
+                }
 
-                RaycastHit raycastHit = new RaycastHit();
-                Physics.Linecast(player.Fireport.position, player.Fireport.position - player.Fireport.up * 1000f, out raycastHit, 331776);
+                Physics.Linecast(player.Fireport.position, player.Fireport.position - player.Fireport.up * 1000f, out var raycastHit, 331776);
                 return raycastHit.point;
             }
-            catch
+            catch (Exception e)
             {
+                Log.Error($"Coordinate Debug raycast failed: {e.Message}");
                 return Vector3.zero;
             }
         }
