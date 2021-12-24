5a66a39d0e9579696558d522cf083359d93f19ad
Pedro Silva <sparta14gaming@gmail.com>
2021-12-24 00:14:55 +0000

    Fixed long game loading time, updated dependencies
     - Updated HarmonyX, MonoMod.RuntimeDetour and MonoMod.Utils to the latest versions;
     - Switched EasyBundlePatch PatchPrefix to PatchPostfix.
     - Fixed a problem with the initial game loading time sometimes taking 2 - 3 minutes or more;

--- project/Aki.Bundles/Patches/EasyBundlePatch.cs
+++ project/Aki.Bundles/Patches/EasyBundlePatch.cs
@@ -24,27 +24,17 @@ namespace Aki.Bundles.Patches
             return EasyBundleHelper.Type.GetConstructors()[0];
         }
 
-        [PatchPrefix]
-        private static bool PatchPrefix(object __instance, string key, string rootPath, CompatibilityAssetBundleManifest manifest, IBundleLock bundleLock)
+        [PatchPostfix]
+        private static void PatchPostfix(object __instance, string key, string rootPath, CompatibilityAssetBundleManifest manifest, IBundleLock bundleLock)
         {
             var path = rootPath + key;
             var dependencyKeys = manifest.GetDirectDependencies(key);
 
             if (BundleSettings.Bundles.TryGetValue(key, out BundleInfo bundle))
             {
-                path = bundle.Path;
-            }
-
-            foreach (KeyValuePair<string, BundleInfo> kvp in BundleSettings.Bundles)
-            {
-                if (!key.Equals(kvp.Key))
-                {
-                    continue;
-                }
-
                 var result = dependencyKeys == null ? new List<string>() : dependencyKeys.ToList();
-                dependencyKeys = result.Union(kvp.Value.DependencyKeys).ToArray();
-                break;
+                dependencyKeys = result.Union(bundle.DependencyKeys).ToArray();
+                path = bundle.Path;
             }
 
             _ = new EasyBundleHelper(__instance)
@@ -56,8 +46,6 @@ namespace Aki.Bundles.Patches
                 LoadState = new BindableState<ELoadState>(ELoadState.Unloaded, null),
                 BundleLock = bundleLock
             };
-
-            return false;
         }
     }
 }
