b509508ddea989965146b1a252d6973d63097f40
CWX <CWXDEV@outlook.com>
2022-01-05 12:28:56 +0000

    removed a comment/ added one to explain its in seconds for easy reading

--- project/Aki.Custom/Patches/AfkTimerPatch.cs
+++ project/Aki.Custom/Patches/AfkTimerPatch.cs
@@ -13,21 +13,22 @@ namespace Aki.Custom.Patches
         {
             var type = PatchConstants.EftTypes.Single(x => x.Name == "MainApplication");
             return Array.Find(type.GetMethods(PatchConstants.PrivateFlags), IsTargetMethod);
-
         }
 
         private static bool IsTargetMethod(MethodInfo mi)
-        {   // 16432 this is method_22
+        {   
             var parameters = mi.GetParameters();
             return parameters.Length == 1
                 && parameters[0].Name == "afkTimeOut";
         }
 
-        [PatchPrefix]
+        [PatchPrefix] 
         private static bool PatchPrefix(ref float afkTimeOut)
         {
             var json = RequestHandler.GetJson("/singleplayer/settings/afkTimeOut");
             var isParsable = float.TryParse(json, out afkTimeOut);
+
+            // time is in seconds
             afkTimeOut = isParsable ? afkTimeOut : 86400;
             return true;
         }
