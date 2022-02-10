5b73dc7565ea13d31b331fe2c2ff58dec4181834
Chomp <chomp@noreply.dev.sp-tarkov.com>
2022-02-10 15:23:27 +0000

    update modules solution for 16770 eft

--- project/Aki.SinglePlayer/Patches/ScavMode/LoadOfflineRaidScreenPatch.cs
+++ project/Aki.SinglePlayer/Patches/ScavMode/LoadOfflineRaidScreenPatch.cs
@@ -112,7 +112,7 @@ namespace Aki.SinglePlayer.Patches.ScavMode
         private static void LoadOfflineRaidScreenForScav()
         {
             var menuController = (object)GetMenuController();
-            var gclass = new MatchmakerOfflineRaid.GClass2423();
+            var gclass = new MatchmakerOfflineRaid.GClass2424();
 
             gclass.OnShowNextScreen += LoadOfflineRaidNextScreen;
 
