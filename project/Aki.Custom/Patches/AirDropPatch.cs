5ad303275e4d191ff5f0324695321f804e62eed3
CWX <CWXDEV@outlook.com>
2022-04-26 14:48:06 +0100

    updated to attach new component to gameworld when appropriate

--- project/Aki.Custom/Patches/AirDropPatch.cs
+++ project/Aki.Custom/Patches/AirDropPatch.cs
@@ -11,8 +11,7 @@ namespace Aki.Custom.Patches
 {
     public class AirDropPatch : ModulePatch
     {
-        private static GameObject gameObject = null;
-        private static GameWorld gameWorld = null;
+        public static GameWorld gameWorld = null;
 
         protected override MethodBase GetTargetMethod()
         {
@@ -22,30 +21,15 @@ namespace Aki.Custom.Patches
         [PatchPostfix]
         public static void PatchPostFix()
         {
-            if (gameObject == null)
-            {
-                gameObject = new GameObject
-                {
-                    name = "Aki.AirDrop"
-                };
-            }
+            gameWorld = Singleton<GameWorld>.Instance;
 
             if (gameWorld != null)
             {
+                var player = gameWorld.RegisteredPlayers[0];
+                if (player != null && (player.Location.Contains("factory") || player.Location.Contains("laboratory") || player.Location.Contains("hideout")))
+                return;
 
-                AirDrop component = gameObject.GetComponent<AirDrop>();
-
-                if (component == null)
-                {
-                    gameObject.AddComponent<AirDrop>();
-                    UnityEngine.Object.DontDestroyOnLoad(gameObject);
-                }
-                else
-                {
-                    gameObject.GetComponent<AirDrop>().OnDestroy();
-                    gameObject.AddComponent<AirDrop>();
-                    UnityEngine.Object.DontDestroyOnLoad(gameObject);
-                }
+                gameWorld.GetOrAddComponent<AirDrop>();
             }
         }
     }
@@ -77,13 +61,13 @@ namespace Aki.Custom.Patches
         {
             planeEnabled = false;
             boxEnabled = false;
-            dropChance = 20;
+            dropChance = 100; // 20
             boxObjId = 10;
             planePositivePosition = 3000f;
             planeNegativePosition = -3000f;
             defaultDropHeight = 400f;
             doNotRun = false;
-            timeToDrop = RandomChanceGen(60, 900);
+            timeToDrop = RandomChanceGen(60, 70); // 60, 900
             planeObjId = RandomChanceGen(1, 4);
             plane = LocationScene.GetAll<SynchronizableObject>().First(x => x.name.Contains("IL76MD-90"));
             box = LocationScene.GetAll<SynchronizableObject>().First(x => x.name.Contains("scontainer_airdrop_box_04"));
@@ -95,66 +79,40 @@ namespace Aki.Custom.Patches
         public void FixedUpdate()
         {
             if (gameWorld == null)
-            {
-                return;
-            }
+            return;
 
             timer += 0.02f;
-
             if (timer >= timeToDrop && !planeEnabled && amountDropped != 1 && !doNotRun)
-            {
-                ScriptStart();
-            }
+            ScriptStart();
 
             if (timer >= timeToDrop && planeEnabled && !doNotRun)
             {
-                plane.transform.Translate(Vector3.forward, Space.Self); // transform foward on local rotation using Space.Self, vector3.forward moves forward 1f per run
-
+                plane.transform.Translate(Vector3.forward, Space.Self);
                 switch (planeObjId)
                 {
                     case 1:
-                        if (plane.transform.position.z >= planePositivePosition && planeEnabled) // spawn 1 moves along Z axis and goes in the positive direction, 
-                        {
-                            DisablePlane();
-                        }
-
+                        if (plane.transform.position.z >= planePositivePosition && planeEnabled)
+                        DisablePlane();
                         if (plane.transform.position.z >= randomAirdropPoint.transform.position.z && !boxEnabled)
-                        {
-                            InitDrop();
-                        }
+                        InitDrop();
                         break;
                     case 2:
-                        if (plane.transform.position.x >= planePositivePosition && planeEnabled) // spawn 2 moves along x axis and goes in the positive direction
-                        {
-                            DisablePlane();
-                        }
-
+                        if (plane.transform.position.x >= planePositivePosition && planeEnabled)
+                        DisablePlane();
                         if (plane.transform.position.x >= randomAirdropPoint.transform.position.x && !boxEnabled)
-                        {
-                            InitDrop();
-                        }
+                        InitDrop();
                         break;
                     case 3:
-                        if (plane.transform.position.z <= planeNegativePosition && planeEnabled) // spawn 3 moves along z axis and goes in the negative direction
-                        {
-                            DisablePlane();
-                        }
-
+                        if (plane.transform.position.z <= planeNegativePosition && planeEnabled)
+                        DisablePlane();
                         if (plane.transform.position.z <= randomAirdropPoint.transform.position.z && !boxEnabled)
-                        {
-                            InitDrop();
-                        }
+                        InitDrop();
                         break;
                     case 4:
-                        if (plane.transform.position.x <= planeNegativePosition && planeEnabled) // spawn 2 moves along x axis and goes in the negative direction
-                        {
-                            DisablePlane();
-                        }
-
+                        if (plane.transform.position.x <= planeNegativePosition && planeEnabled)
+                        DisablePlane();
                         if (plane.transform.position.x <= randomAirdropPoint.transform.position.x && !boxEnabled)
-                        {
-                            InitDrop();
-                        }
+                        InitDrop();
                         break;
                 }
             }
@@ -162,22 +120,12 @@ namespace Aki.Custom.Patches
 
         public bool ShouldAirdropOccur()
         {
-            var player = gameWorld.RegisteredPlayers[0];
-            if (player != null)
-            {
-                if (player.Location.Contains("factory") || player.Location.Contains("laboratory") || player.Location.Contains("hideout"))
-                {
-                    DoNotRun(); // will stay true till next map load
-                    return false;
-                }
-            }
-
             return (RandomChanceGen(1, 99) <= dropChance);
         }
 
         public void DoNotRun()
         {
-            doNotRun = false;
+            doNotRun = true;
         }
 
         public int RandomChanceGen(int minValue, int maxValue)
@@ -189,20 +137,20 @@ namespace Aki.Custom.Patches
 
         public void ScriptStart()
         {
-            if (ShouldAirdropOccur())
+            if (!ShouldAirdropOccur())
             {
-
-                if (box != null)
-                {
-                    boxPosition = randomAirdropPoint.transform.position;
-                    boxPosition.y = defaultDropHeight;
-                }
-
-                if (plane != null)
-                {
-                    PlanePositionGen();
-                }
+                DoNotRun();
+                return;
             }
+
+            if (box != null)
+            {
+                boxPosition = randomAirdropPoint.transform.position;
+                boxPosition.y = defaultDropHeight;
+            }
+
+            if (plane != null)
+            PlanePositionGen();
         }
 
         public void InitPlane()
@@ -210,18 +158,17 @@ namespace Aki.Custom.Patches
             planeEnabled = true;
             plane.TakeFromPool();
             plane.Init(planeObjId, planeStartPosition, planeStartRotation);
-            plane.transform.LookAt(boxPosition); // turns the plane to look at the box drop point
+            plane.transform.LookAt(boxPosition);
             plane.ManualUpdate(0);
 
             var sound = plane.GetComponentInChildren<AudioSource>();
             sound.volume = 1f;
-            sound.dopplerLevel = 1;
+            sound.dopplerLevel = 0;
             sound.Play();
         }
 
         public void InitDrop()
         {
-            // this is basically a copy of the debugAirdrop script BSG has in the syncProcess
             object[] objToPass = new object[1];
             objToPass[0] = SynchronizableObjectType.AirDrop;
             gameWorld.SynchronizableObjectLogicProcessor.GetType().GetMethod("method_9", BindingFlags.NonPublic | BindingFlags.Instance)
